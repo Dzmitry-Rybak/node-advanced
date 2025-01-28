f23b44a4c5d2bd9c2c7d7522a4b032fc3227f8a5
Dzmitry Rybak <dzmitry.rybak@capgemini.com>
2025-01-28 09:10:09 +0100

    worker-threads

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const cluster = require("cluster");
 const crypto = require("crypto");
+const Worker = require("worker-threads".WOrker);
 
-// Is the file being executed in master mode
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but
-  // in child mode
-  cluster.fork();
-} else {
-  // Im a child, im going to act like a server and do nothing else
-  const express = require("express");
-  const app = express();
+const express = require("express");
+const app = express();
 
-  app.get("/", (req, res) => {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => res.send("hi there"));
+app.get("/", (req, res) => {
+  const worker = new Worker(function () {
+    this.onmessage = function () {
+      let counter = 0;
+      while (counter < 1e9) {
+        counter++;
+      }
+    };
+
+    postMessage(counter);
   });
 
-  app.get("/fast", (req, res) => {
-    res.send("This was fast!");
-  });
+  worker.onmessage = function (message) {
+    console.log(message.data);
+    res.send(" " + message.data);
+  };
 
-  app.listen(3000);
-}
+  worker.postMessage();
+});
+
+app.get("/fast", (req, res) => {
+  res.send("This was fast!");
+});
+
+app.listen(3000);
